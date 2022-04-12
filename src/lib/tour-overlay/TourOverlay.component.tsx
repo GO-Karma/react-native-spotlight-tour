d0896ee0304570835d817440c8633e445d787b5a
Aykut Saki <aykutsakibusiness@gmail.com>
2022-04-12 16:34:36 +0300

    lint

--- src/lib/tour-overlay/TourOverlay.component.tsx
+++ src/lib/tour-overlay/TourOverlay.component.tsx
@@ -4,10 +4,17 @@ import {
   LayoutRectangle,
   Modal,
   StyleProp,
-  ViewStyle
+  ViewStyle,
 } from "react-native";
 import Svg, { Defs, Mask, Rect, RectProps, rgbaArray } from "react-native-svg";
-import ReAnimated, { Easing, useAnimatedProps, useAnimatedStyle, useSharedValue, withDelay, withTiming } from "react-native-reanimated"
+import ReAnimated, {
+  Easing,
+  useAnimatedProps,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from "react-native-reanimated";
 
 import { vhDP, vwDP } from "../../helpers/responsive";
 import { Align, Position, SpotlightTourCtx } from "../SpotlightTour.context";
@@ -25,211 +32,227 @@ interface TourOverlayProps {
 }
 
 // const AnimatedCircle = Animated.createAnimatedComponent(Circle);
-const AnimatedRect = ReAnimated.createAnimatedComponent(Rect)
+const AnimatedRect = ReAnimated.createAnimatedComponent(Rect);
 
-export const TourOverlay = React.forwardRef<TourOverlayRef, TourOverlayProps>((props, ref) => {
-  const { color = "black", opacity = 0.45, tour } = props;
-  const { current, next, previous, spot, steps, stop } = tour;
+export const TourOverlay = React.forwardRef<TourOverlayRef, TourOverlayProps>(
+  (props, ref) => {
+    const { color = "black", opacity = 0.45, tour } = props;
+    const { current, next, previous, spot, steps, stop } = tour;
 
-  if (!spot || current === undefined) {
-    return null;
-  }
-
-  const sizeWidth = useSharedValue(0)
-  const sizeHeight = useSharedValue(0)
-  const locationX = useSharedValue(0)
-  const locationY = useSharedValue(0)
-  const tipOp = useSharedValue(0)
-
-  const [tourStep, setTourStep] = useState(steps[current]);
-  const [tipStyle, setTipStyle] = useState<StyleProp<ViewStyle>>();
-  // const [radius] = useState(new Animated.Value(0));
-  // const [center] = useState(new Animated.ValueXY({ x: 0, y: 0 }));
-  // const [tipOpacity] = useState(new Animated.Value(0));
-
-  const r = (Math.max(spot.width, spot.height) / 2) * 1.15;
-  const cx = spot.x + (spot.width / 2);
-  const cy = spot.y + (spot.height / 2);
-
-  const xLoc = spot.x
-  const yLoc = spot.y
-  const width = spot.width
-  const height = spot.height
-
-  // const useNativeDriver = useMemo(() => Platform.select({
-  //   android: false,
-  //   default: false,
-  //   ios: true
-  // }), [Platform.OS]);
-
-  const getTipStyles = (tipLayout: LayoutRectangle): StyleProp<ViewStyle> => {
-    const tipMargin: string = "2%";
-    const align = tourStep.alignTo ?? Align.SPOT;
-
-    switch (tourStep.position) {
-      case Position.BOTTOM: return {
-        left: align === Align.SPOT
-          ? Math.round(cx - (tipLayout.width / 2))
-          : Math.round((vwDP(100) - tipLayout.width) / 2),
-        marginTop: tipMargin,
-        top: Math.round(cy + r)
-      };
-
-      case Position.TOP: return {
-        left: align === Align.SPOT
-          ? Math.round(cx - (tipLayout.width / 2))
-          : Math.round((vwDP(100) - tipLayout.width) / 2),
-        marginBottom: tipMargin,
-        top: Math.round(cy - r - tipLayout.height)
-      };
-
-      case Position.LEFT: return {
-        left: Math.round(cx - r - tipLayout.width),
-        marginRight: tipMargin,
-        top: Math.round(cy - (tipLayout.height / 2))
-      };
-
-      case Position.RIGHT: return {
-        left: Math.round(cx + r),
-        marginLeft: tipMargin,
-        top: Math.round(cy - (tipLayout.height / 2))
-      };
+    if (!spot || current === undefined) {
+      return null;
     }
-  };
 
-  const measureTip = (event: LayoutChangeEvent) => {
-    setTipStyle(getTipStyles(event.nativeEvent.layout));
-  };
+    const sizeWidth = useSharedValue(0);
+    const sizeHeight = useSharedValue(0);
+    const locationX = useSharedValue(0);
+    const locationY = useSharedValue(0);
+    const tipOp = useSharedValue(0);
 
-  useEffect(() => {
-    // const moveIn = Animated.parallel([
-    //   Animated.spring(center, {
-    //     damping: 50,
-    //     mass: 5,
-    //     stiffness: 300,
-    //     toValue: { x: cx, y: cy },
-    //     useNativeDriver
-    //   }),
-    //   Animated.spring(radius, {
-    //     damping: 30,
-    //     mass: 5,
-    //     stiffness: 300,
-    //     toValue: r,
-    //     useNativeDriver
-    //   }),
-    //   Animated.timing(tipOpacity, {
-    //     delay: 500,
-    //     duration: 500,
-    //     toValue: 1,
-    //     useNativeDriver
-    //   })
-    // ]);
+    const [tourStep, setTourStep] = useState(steps[current]);
+    const [tipStyle, setTipStyle] = useState<StyleProp<ViewStyle>>();
+    // const [radius] = useState(new Animated.Value(0));
+    // const [center] = useState(new Animated.ValueXY({ x: 0, y: 0 }));
+    // const [tipOpacity] = useState(new Animated.Value(0));
 
-    // moveIn.stop();
-    setTourStep(steps[current]);
+    const r = (Math.max(spot.width, spot.height) / 2) * 1.15;
+    const cx = spot.x + spot.width / 2;
+    const cy = spot.y + spot.height / 2;
 
-    /**
-     * We need to start the animation asynchronously or the layout callback may
-     * overlap, causing different behaviors in iOS and than Android.
-     * TODO: Refactor the animation flow to better handle the layout callback.
-     */
-    setTimeout(() => {
-      setTipStyle(undefined);
-      sizeWidth.value = withTiming(width + 10, { duration: 500,
-  easing: Easing.bezier(0.33, 0.01, 0, 1) })
-      sizeHeight.value = withTiming(height + 10, { duration: 500,
-  easing: Easing.bezier(0.33, 0.01, 0, 1) })
-      locationX.value = withTiming(xLoc - 5, { duration: 500,
-  easing: Easing.bezier(0.33, 0.01, 0, 1) })
-      locationY.value = withTiming(yLoc - 5, { duration: 500,
-  easing: Easing.bezier(0.33, 0.01, 0, 1) })
-      tipOp.value = withDelay(500, withTiming(1, { duration: 500 }))
-      // moveIn.start();
-    });
-  }, [spot, current]);
+    const xLoc = spot.x;
+    const yLoc = spot.y;
+    const width = spot.width;
+    const height = spot.height;
 
-  useImperativeHandle(ref, () => ({
-    hideTip() {
-      return new Promise<void>((resolve) => {
-        // Animated.timing(tipOpacity, {
-        //   duration: 200,
-        //   toValue: 0,
-        //   useNativeDriver
-        // })
-        // .start(({ finished }) => finished
-        //   ? resolve()
-        //   : reject()
-        // );
-        tipOp.value = withTiming(0, { duration: 200 })
-        setTimeout(() => {
-          resolve()
-        }, 200)
+    // const useNativeDriver = useMemo(() => Platform.select({
+    //   android: false,
+    //   default: false,
+    //   ios: true
+    // }), [Platform.OS]);
+
+    const getTipStyles = (tipLayout: LayoutRectangle): StyleProp<ViewStyle> => {
+      const tipMargin: string = "2%";
+      const align = tourStep.alignTo ?? Align.SPOT;
+
+      switch (tourStep.position) {
+        case Position.BOTTOM:
+          return {
+            left:
+              align === Align.SPOT
+                ? Math.round(cx - tipLayout.width / 2)
+                : Math.round((vwDP(100) - tipLayout.width) / 2),
+            marginTop: tipMargin,
+            top: Math.round(cy + r),
+          };
+
+        case Position.TOP:
+          return {
+            left:
+              align === Align.SPOT
+                ? Math.round(cx - tipLayout.width / 2)
+                : Math.round((vwDP(100) - tipLayout.width) / 2),
+            marginBottom: tipMargin,
+            top: Math.round(cy - r - tipLayout.height),
+          };
+
+        case Position.LEFT:
+          return {
+            left: Math.round(cx - r - tipLayout.width),
+            marginRight: tipMargin,
+            top: Math.round(cy - tipLayout.height / 2),
+          };
+
+        case Position.RIGHT:
+          return {
+            left: Math.round(cx + r),
+            marginLeft: tipMargin,
+            top: Math.round(cy - tipLayout.height / 2),
+          };
+      }
+    };
+
+    const measureTip = (event: LayoutChangeEvent) => {
+      setTipStyle(getTipStyles(event.nativeEvent.layout));
+    };
+
+    useEffect(() => {
+      // const moveIn = Animated.parallel([
+      //   Animated.spring(center, {
+      //     damping: 50,
+      //     mass: 5,
+      //     stiffness: 300,
+      //     toValue: { x: cx, y: cy },
+      //     useNativeDriver
+      //   }),
+      //   Animated.spring(radius, {
+      //     damping: 30,
+      //     mass: 5,
+      //     stiffness: 300,
+      //     toValue: r,
+      //     useNativeDriver
+      //   }),
+      //   Animated.timing(tipOpacity, {
+      //     delay: 500,
+      //     duration: 500,
+      //     toValue: 1,
+      //     useNativeDriver
+      //   })
+      // ]);
+
+      // moveIn.stop();
+      setTourStep(steps[current]);
+
+      /**
+       * We need to start the animation asynchronously or the layout callback may
+       * overlap, causing different behaviors in iOS and than Android.
+       * TODO: Refactor the animation flow to better handle the layout callback.
+       */
+      setTimeout(() => {
+        setTipStyle(undefined);
+        sizeWidth.value = withTiming(width + 10, {
+          duration: 500,
+          easing: Easing.bezier(0.33, 0.01, 0, 1),
+        });
+        sizeHeight.value = withTiming(height + 10, {
+          duration: 500,
+          easing: Easing.bezier(0.33, 0.01, 0, 1),
+        });
+        locationX.value = withTiming(xLoc - 5, {
+          duration: 500,
+          easing: Easing.bezier(0.33, 0.01, 0, 1),
+        });
+        locationY.value = withTiming(yLoc - 5, {
+          duration: 500,
+          easing: Easing.bezier(0.33, 0.01, 0, 1),
+        });
+        tipOp.value = withDelay(500, withTiming(1, { duration: 500 }));
+        // moveIn.start();
       });
-    }
-  }));
+    }, [spot, current]);
 
-  const animatedProps = useAnimatedProps<RectProps>(() => ({
-    width: sizeWidth.value,
-    height: sizeHeight.value,
-    x: locationX.value,
-    y: locationY.value,
-    fill: "black",
-    rx: 5
-  }))
+    useImperativeHandle(ref, () => ({
+      hideTip() {
+        return new Promise<void>((resolve) => {
+          // Animated.timing(tipOpacity, {
+          //   duration: 200,
+          //   toValue: 0,
+          //   useNativeDriver
+          // })
+          // .start(({ finished }) => finished
+          //   ? resolve()
+          //   : reject()
+          // );
+          tipOp.value = withTiming(0, { duration: 200 });
+          setTimeout(() => {
+            resolve();
+          }, 200);
+        });
+      },
+    }));
 
-  const tipOpacityStyle = useAnimatedStyle(() => ({
-    opacity: tipOp.value
-  }))
+    const animatedProps = useAnimatedProps<RectProps>(() => ({
+      width: sizeWidth.value,
+      height: sizeHeight.value,
+      x: locationX.value,
+      y: locationY.value,
+      fill: "black",
+      rx: 5,
+    }));
 
-  return (
-    <Modal
-      animationType="fade"
-      presentationStyle="overFullScreen"
-      transparent={true}
-      visible={true}>
-      <OverlayView accessibilityLabel="Tour Overlay View">
-        <Svg
-          accessibilityLabel="Svg overlay view"
-          height="100%"
-          width="100%"
-          viewBox={`0 0 ${vwDP(100)} ${vhDP(100)}`}>
-          <Defs>
-            <Mask id="mask" x={0} y={0} height="100%" width="100%">
-              <Rect height="100%" width="100%" fill="#fff" />
-              {/* <AnimatedCircle
+    const tipOpacityStyle = useAnimatedStyle(() => ({
+      opacity: tipOp.value,
+    }));
+
+    return (
+      <Modal
+        animationType="fade"
+        presentationStyle="overFullScreen"
+        transparent={true}
+        visible={true}>
+        <OverlayView accessibilityLabel="Tour Overlay View">
+          <Svg
+            accessibilityLabel="Svg overlay view"
+            height="100%"
+            width="100%"
+            viewBox={`0 0 ${vwDP(100)} ${vhDP(100)}`}>
+            <Defs>
+              <Mask id="mask" x={0} y={0} height="100%" width="100%">
+                <Rect height="100%" width="100%" fill="#fff" />
+                {/* <AnimatedCircle
                 r={radius}
                 cx={center.x}
                 cy={center.y}
                 fill="black"
               /> */}
 
-              <AnimatedRect animatedProps={animatedProps} />
-            </Mask>
-          </Defs>
+                <AnimatedRect animatedProps={animatedProps} />
+              </Mask>
+            </Defs>
 
-          <Rect
-            height="100%"
-            width="100%"
-            fill={color}
-            mask="url(#mask)"
-            opacity={opacity}
-          />
-        </Svg>
+            <Rect
+              height="100%"
+              width="100%"
+              fill={color}
+              mask="url(#mask)"
+              opacity={opacity}
+            />
+          </Svg>
 
-        <TipView
-          accessibilityLabel="Tip Overlay View"
-          onLayout={measureTip}
-          style={[tipStyle, tipOpacityStyle]}>
-          {tourStep.render({
-            current,
-            isFirst: current === 0,
-            isLast: current === steps.length - 1,
-            next,
-            previous,
-            stop,
-          })}
-        </TipView>
-      </OverlayView>
-    </Modal>
-  );
-});
+          <TipView
+            accessibilityLabel="Tip Overlay View"
+            onLayout={measureTip}
+            style={[tipStyle, tipOpacityStyle]}>
+            {tourStep.render({
+              current,
+              isFirst: current === 0,
+              isLast: current === steps.length - 1,
+              next,
+              previous,
+              stop,
+            })}
+          </TipView>
+        </OverlayView>
+      </Modal>
+    );
+  }
+);
